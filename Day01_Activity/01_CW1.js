5ac9eea452ff89036e0a1a87b9e01fc5fa3db61e
sarunjebaraj <s.arunjebaraj@gmail.com>
2025-11-25 22:34:42 +0530

    Add files via upload
    Day1

---
+++ Day01_Activity/01_CW1.js
@@ -0,0 +1,27 @@
+
+//Variable declaration with LET
+
+//firstname
+let firstname = "Arun"
+console.log(firstname);
+console.log(typeof firstname);
+
+//companyname
+let companyname = "CTS"
+console.log(companyname);
+console.log(typeof companyname);
+
+//mobileNumber
+let mobileNumber = 9875632145
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+//isAutomation
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//hasPlaywright
+let hasPlaywright = 
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
---
+++ Day01_Activity/02_CW2.js
@@ -0,0 +1,26 @@
+//Variable declaration with VAR
+
+//firstname
+var firstname = "Arun"
+console.log(firstname);
+console.log(typeof firstname);
+
+//companyname
+var companyname = "CTS"
+console.log(companyname);
+console.log(typeof companyname);
+
+//mobileNumber
+var mobileNumber = 9875632145
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+//isAutomation
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//hasPlaywright
+var hasPlaywright = 
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
